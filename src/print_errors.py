3180502f41f5f0a5ce1966f0db3e49095008a8cd
Abdon Verdejo-Parada <averdejo02@utexas.edu>
2022-05-07 21:43:48 -0500

    Added a new error message incase the user wants a list of information concerning an asteroid and db=0 is empty, db=0 needs to be populated first before anything can be done

--- src/print_errors.py
+++ src/print_errors.py
@@ -19,7 +19,8 @@ def error(correct_curl):
     '''
     return f'\n\n[Error]: Invalid curl for this route. Here is the correct command for this route.\n     {correct_curl}\nRetype the command above to get the correct results.\n\n'
 
-    
+
+#This function will print the welcome message to the user    
 def welcome_message():
     '''
     This function will print the welcome message to the user, that contains all the routes that this api supports.
@@ -39,11 +40,36 @@ Instructions on how to use this application, to store and delete data:
 
 /                GET     Shows all the routes that the user is able to call
 /data            POST    Uploads data into database
+/data/read       GET     Returns a list of all the items that were put in the database
 /data/reset      DELETE  Resets the db that stores the data of the database
+
 
 '''
     
+#This function will print an error message telling the user that db=0 is empty
+def db0_is_empty(current_route):
+    """
+    This function will return a message to the user telling them  that db=0 is empty.
 
+    Input:
+        current_route (str) It is the current route that was just curled.
+
+    Output:
+        (string): It will be a string that is returned to the user as an error message
+    """
+
+
+    return f"""
+
+[Error]: The database that stores all of the data in the redis container is empty.
+Nothing can be returned to the user until the database is filled. In order to populate the database
+use the following command:
+                curl -X POST localhost:5036/data
+With that comamnd completed, you can redo the following command once again:
+                {current_route}
+
+"""
 
 print(error('curl -X GET localhost:5036'))
 print(welcome_message())
+print(db0_is_empty('curl -X POST localhost:5036/data'))

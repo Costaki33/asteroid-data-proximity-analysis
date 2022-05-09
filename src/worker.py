16bddd24d8ccf49a75f734e3e8beeb73285eebd9
Abdon Verdejo-Parada <averdejo02@utexas.edu>
2022-05-09 02:35:55 -0500

    added a few more functions that will be used to do the work

--- src/worker.py
+++ src/worker.py
@@ -2,21 +2,56 @@ import jobs
 import time
 import json
 from flask import Flask, request, jsonify
+import logging
 
+logging.basicConfig(level=logging.DEBUG)
 #These are all the functions that will do various jobs
 
 #This function will return a list of dictionaries to the user for every asteroid entry
 def read_data(jid):
+    '''
+    This function will return a list of dictionaries to the user for every asteroid entry 
+
+    Input:
+       jid (string) It is the job id that was created randomly.
+
+    Output:
+       (none)
+    '''
     list_of_data = []
     for item in jobs.rd.keys():
         #json.loads turns a json string to a dictionary
         list_of_data.append(json.loads(jobs.rd.get(item)))
 
     #Add this item into the answers db=3. the key value will be jid and the value will be the return
+    
     jobs.answers.set(jid, json.dumps(list_of_data))
     
 
+#This function will return a list that contains all of the ids from the dataset
+def list_ids(jid):
+    """
+    This function will return a list that contains all the id's in the database.
 
+    Input:
+       jid (str): It is the job id
+
+    Output:
+       (None)
+    """
+    id_list = ["blah"]
+    
+    logging.warning('Before going into the for loop')
+    #going through entire dataset to find ids
+    for item in jobs.rd.keys():
+        currentdict = json.loads(jobs.rd.get(item))
+        
+        # Adding 'id' into the list
+        id_list.append(currentdict['id'])
+        
+    #adding the return to the answers db
+    jobs.answers.set(jid, json.dumps(id_list))
+    
 
 @jobs.q.worker
 def execute_job(jid):
@@ -35,7 +70,11 @@ def execute_job(jid):
 
     #depending on the route, it will call different functions that will return different results
     if(route == '/job/data/read'):
+        logging.warning('inside the /job/data/read if statement')
         read_data(jid)
+    elif(route == '/job/ids'):
+        logging.warning('inside the /job/ids if statement')
+        list_ids(jid)
         
     #There will be a 15 second buffer for the program to the job, during this time worker will the work
     time.sleep(15)

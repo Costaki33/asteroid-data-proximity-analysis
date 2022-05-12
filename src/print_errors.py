0c575948c5f0a0b90c1c25aa758bf56de247c120
Costaki <cskevofilax@utexas.edu>
2022-05-12 00:24:01 -0500

    updated print_errors, almost complete

--- src/print_errors.py
+++ src/print_errors.py
@@ -1,157 +1,192 @@
 '''
-This file will contian all the error messages and other items that will be printed to the user
+
+    This file will contian all the error messages and other items that will be printed to the user
+
 '''
 
-#This will print the correct curling command to the user
+# This function will print the correct curling command to the user
 def error(correct_curl):
+
     '''
+    
     This function will return the correct curling command for each curl command that was incorrectly inputted by
     the user.
 
     Input:
-        correct_curl (string) it is the correct command for that specific route
+        correct_curl (string): This string is the correct command for that specific route the user is calling
 
     Output:
-        (string) it will be the correct command for the user to use.
+        Error message (string): This output string message will be the correct command for the user to use
 
     '''
-    return f'\n\n[Error]: Invalid curl for this route. Here is the correct command for this route.\n     {correct_curl}\nRetype the command above to get the correct results.\n\n'
+
+    return f'\n\n[ERROR]: Invalid curl command for this route. Here is the correct command for this route:\n{correct_curl}\nRetype your command above to get the results you seek.\n\n'
 
 
-#This function will print the welcome message to the user
+# This function prints the welcome message to the user
 def welcome_message():
+    
     '''
-    This function will print the welcome message to the user, that contains all the routes that this api supports.
+    
+    This function will print a welcome message to the user, which contains all the routes that this API program supports
 
     Input:
         (none)
 
     Output:
-        (string) it will be the string that will be shown to the user.
+        Welcome mesage (string): This output string message will be a string that will be shown to the user with instructions 
 
     '''
+
     return f'''
 
-####Asteroid-data-proximity####
+--- Asteroid Proximity Data Query Program ---
 
-Instructions on how to use this application, to store and delete data:
+Welcome to the Asteroid Proximity Data Query System! With this application, you can query different datasets about asteroids that are close to Earth. 
+-> Below are instructions on how to store and delete data:
 
-/                GET     Shows all the routes that the user is able to call
-/data            POST    Uploads data into database
-/data/reset      DELETE  Resets the db that stores the data of the database
+COMMAND:         HTTP METHOD:
+-------------------------------------------------------------------------------------
+/                GET     [This route shows all the available commands you can utilize]
+/data            POST    [Uploads the data into local database]
+/data/reset      DELETE  [Resets the db container that stores the data that is currently in the database]
 
-Instructions on how to get the API to return things back to the user
+-> Further instructions on how to get the API application to return data back to you, the user:
 
-/data/read       GET     Returns a list of all the items that were put in the database
+
+COMMAND:         HTTP METHOD:
+-------------------------------------------------------------------------------------
+/data/read       GET     [This route returns a list of all the items that were put into the local database]
+
 
 '''
     
 
-#This function will print an error message telling the user that db=0 is empty
+# This function prints out an error message, notifying the user that db = 0 is empty
 def db0_is_empty(current_route):
-    """
-    This function will return a message to the user telling them  that db=0 is empty.
+    
+    '''
+    
+    This function return an error message to the user, notifying them that db = 0 is empty
 
     Input:
-        current_route (str) It is the current route that was just curled.
+        current_route (str): This string is the current route that was just curled by the user
 
     Output:
-        (string): It will be a string that is returned to the user as an error message
-    """
+        Error message (string): This output error message is a string that is returned to the user
+
+    '''
 
 
-    return f"""
+    return f'''
 
-[Error]: The database that stores all of the data in the redis container is empty.
-Nothing can be returned to the user until the database is filled. In order to populate the database
+[ERROR]: The database that stores all of the data in the Redis container is empty.
+Because of this, nothing can be returned to the user until the database has been filled. In order to populate the database,
 use the following command:
-                curl -X POST localhost:5036/data
-With that comamnd completed, you can redo the following command once again:
+                curl -X POST localhost:5031/data
+Following that completed & successful command, you can redo the following command:
                 {current_route}
 
-"""
+'''
 
 
-#This function will print an error message, telling the user that the list is empty
+# This function prints an error message, notifying the user that the list is empty
 def list_if_empty(current_route):
+    
     '''
+    
     This function will print to the user that the list is empty.
 
     Input:
-       (current_route) (string): It is the current route that is the user currently curled.
+       (current_route) (string): The current route that is the user currently curling
 
     Output:
-        (string): It will be a string that tells that the list at is empty, to prevent any errors
-        from happening when running the api.
+        Error message (string): A string that tells the user that the pulled list is empty, with the intention of preventing any errors
+        from occuring when running the API 
+    
     '''
     return f'''
 
-The list that will be returned is vacant, meaning that the current route:
+[ERROR]: The requested list that will be returned is vacant, meaning that the current route:
                  {current_route}
-Is going to return an empty list, and an error might be raised.
+Will return an empty list, leading to a possible error being raised.
 
 '''
 
-
+# This function returns a informational message that the user their requested service has been added to the job queue and is working the background
 def job_confi(correct_curl, jid):
+    
     '''
-    This function will return a string to te user telling them that the job has been added to the
-    the queue, and that it will be worked on in the background.
+    
+    This function returns a string notifying the user that their job request has been added to the
+    the Job queue, and it is being worked on in the background
 
     Input:
-        correct_curl (str): That is the curl command inputted by the user
-        jid (str): It is the job id that was randomly generated.
+        correct_curl (str): This string is the curl command inputted by the user
+        jid (str): It is the job id that was randomly generated for the user to call their specific job request
 
     Output:
-        (string): IT will return a confirmation message to the user, letting them know that
-        the job was been put in the queue.
+        Informational mesage(string): A string message confirming that their job was been added to the queue
 
     '''
     return f'''
 
-The job has been succesfully been added to the queue for the folling curl:
+[INFO]: The requested job has been succesfully been added to the job queue for the follwing curl command:
+
                                 {correct_curl}
-The job id for the following job is:
+
+The job id (jid) for the following job is:
+
                                 {jid}
-The jid will be very inportant, in order to get the result of your curl commadn dont lose it.
-If you seem to forget the job id then to the command below to show all the saved job ids
-                                curl -X GET localhost:5036/stored/job-ids
+
+NOTE: The jid is very important. In order to view the results of your curl command, dont lose it.
+
+If you forget the jid, use the following command below to show all the saved job ids:
+                                curl -X GET localhost:5031/stored/job-ids
 
 '''
 
 
-#This function will return a message to the user telling the user that db=4 is empty
+# This function returns a message to the user, notifying them that db = 4 is empty
 def db4_is_empty():
-    """
-    This function will tell the user that the db=4 is empty, meaning that no job has been
+
+    '''
+    
+    This function notifys the user that db = 4 is empty, meaning that no job has been
     instantiated
 
     Input:
         (none)
 
     Output:
-        (string): Telling the user that the db=4 is empty, that a job needs to be instantiated.
-    """
+        Error message (string): Tells the user that the db = 4 is empty. The job needs to be instantiated
+    
+    '''
 
-    return f"""
+    return f'''
 
-The database that contains all the job ids is empty, there must be a job instantiated
-in order to user this route. Try this route again when a job has been instantiated.
+[ERROR]: The database that contains all the job ids is empty. There must be a job instantiated
+in order to use this route. Try using this route again when a job has been instantiated.
+Call '/' to find further commands to instantiate your command.
 
-"""
+'''
 
 
-#This function will tell the user that db=3  is empty
+# This function returns a message to the user, notifying the user that db = 3 is empty
 def db3_is_empty():
-    """
-    This function will tell the user that db=3 is empty
+    
+    '''
+    
+    This function notifys the user that db = 3 is empty, meaning that no job has been
+    instantiated
 
     Input:
        (none)
 
     Output:
        (string) that tells the user that db=3 is empty.
-    """
+    
+    '''
 
     return f'''
 
